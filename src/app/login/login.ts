b5627ee6d90bab2bb9cce499fed3e83998c2798e
Ramon Santos <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2026-02-23 15:41:28 -0400

    preparando los estudiantes

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule,FormsModule],
+  imports: [ReactiveFormsModule, FormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
